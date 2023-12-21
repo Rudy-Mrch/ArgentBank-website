61c0e2686486ef9c0333253e0dd13d44edccbdbb
Rudy-Mrch <rudy.marache@gmail.com>
2023-12-21 08:59:09 +0100

    green code / photo

--- Frontend/my-app/src/pages/Home/Home.js
+++ Frontend/my-app/src/pages/Home/Home.js
@@ -15,20 +15,20 @@ const Home = () => {
   <section className="features">
     <h2 className="sr-only">Features</h2>
    <FeatureItem 
-   iconSrc="/../../img/icon-chat.png"
+   iconSrc="/../../img/icon-chat.webp"
    Alt="Chat Icon"
    title="You are our #1 priorit"
    description=" Need to talk to a representative? You can get in touch through our
    24/7 chat or through a phone call in less than 5 minutes."
    />
       <FeatureItem 
-   iconSrc="/../../img/icon-money.png"
+   iconSrc="/../../img/icon-money.webp"
    Alt="Chat Icon"
    title="More savings means higher rates"
    description=" The more you save with us, the higher your interest rate will be!."
    />
       <FeatureItem 
-   iconSrc="/../../img/icon-security.png"
+   iconSrc="/../../img/icon-security.webp"
    Alt="Chat Icon"
    title="Security you can trust"
    description="  We use top of the line encryption to make sure your data and money
